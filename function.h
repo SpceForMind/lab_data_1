6699edfd456b887d1b1d8656476566c9c6ac7c2f
SpceForMind <31548759+SpceForMind@users.noreply.github.com>
2017-10-16 14:41:45 +0300

    Delete function.h

--- function.h
+++
@@ -1,4 +0,0 @@
-int index_first_negative(int a[20],int size);
-int index_last_negative(int a[20],int size);
-int multi_between_negative(int a[20], int size, int fn, int ln);
-int multi_before_and_after_negative(int a[20], int size, int fn, int ln);
