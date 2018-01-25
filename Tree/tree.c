8378543c2c0b693359035300a74505fb8f02d8a1
SpceForMind <casha_davydov@mail.ru>
2018-01-25 15:19:30 +0300

    Tree_Source_File

--- Tree/tree.c
+++ Tree/tree.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+//#include "tree_types.h"
+//#include "tree_API.h"
+
 #define ERROR -1
+#define MAXNAME 20
 
 typedef struct Item
 {
-	char patname[20];
-	char patkind[20];
+	char patname[MAXNAME];
+	char patkind[MAXNAME];
 } Item;
 
 typedef struct trnode
@@ -55,13 +59,13 @@ void InitializeTree(Tree *pt)
 int ToLeft(const Item *pi1, const Item *pi2)
 {
 	int compl;
-	compl = strncmp(pi1->patname, pi2->patname, 20);
+	compl = strncmp(pi1->patname, pi2->patname, MAXNAME);
 
 	if(compl < 0)
 		return 1;
 	else if(compl == 0)
 	{
-		compl = strncmp(pi1->patkind, pi2->patkind, 20);
+		compl = strncmp(pi1->patkind, pi2->patkind, MAXNAME);
 		
 		if(compl < 0)
 			return 1;
@@ -278,7 +282,7 @@ void PrintNode(Trnode *root)
 
 void PrintTree(Tree *ptree)
 {
-	if(PointerIsNull(ptree))
+	if(PointerIsNull(ptree) || ptree->size == 0)
 		return;
 
 	PrintNode(ptree->root);
@@ -291,6 +295,46 @@ int main()
 
 	InitializeTree(&tree);
 	
+	unsigned int n;
+	if(scanf("%u\n", &n)!= 1)
+		return 0;
+	
+	for(int i = 0; i < n; ++i)
+	{
+		char *patname = (char *)malloc(sizeof(char) * MAXNAME);
+		char *patkind = (char *)malloc(sizeof(char) * MAXNAME);
+	
+		if(PointerIsNull(patname) || PointerIsNull(patkind))
+			break;
+	
+		fgets(patname, MAXNAME, stdin);
+		fgets(patkind, MAXNAME, stdin);
+		(*strstr(patname, "\n")) = 0;
+		(*strstr(patkind, "\n")) = 0;
+		
+		Item *pat = (Item *)malloc(sizeof(Item));
+		
+		if(PointerIsNull(pat))
+			break;
+	
+		strncpy(pat->patname, patname, strlen(patname));
+		strncpy(pat->patkind, patkind, strlen(patkind));
+		
+		Trnode *new_node = MakeNode(pat);
+		
+		if(PointerIsNull(new_node))
+			return 0;
+		
+
+		if(AddNode(new_node, &tree.root) == 1)
+			++tree.size;
+	}
+	
+
+	PrintTree(&tree);
+	DeleteTree(&tree);
+	PrintTree(&tree);
+/*
 	Item pat1 = {"John", "Fish"};
 	Trnode *new_node = MakeNode(&pat1);
 	Item pat2 = {"Maggy", "Fish"};
@@ -311,6 +355,7 @@ int main()
 
 	PrintTree(&tree);
 	DeleteTree(&tree);
+*/
 
 	return 0;
 }
